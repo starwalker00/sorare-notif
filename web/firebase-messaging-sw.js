02787204f163d9ea9a450290c416643f47faacfa
aaa www <aaronwalkerup@gmail.com>
2022-01-07 23:02:35 +0100

    working notifications

---
+++ web/firebase-messaging-sw.js
@@ -0,0 +1,3 @@
+importScripts('https://www.gstatic.com/firebasejs/4.1.3/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.1.3/firebase-messaging.js');
+console.log("in sw page");
